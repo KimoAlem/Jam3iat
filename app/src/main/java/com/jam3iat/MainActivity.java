6546050a7ee247bd064dbf9872727d763a97c683
Karam <noral3in053@gmail.com>
2016-08-02 00:13:06 +0300

    todo

--- app/src/main/java/com/jam3iat/MainActivity.java
+++ app/src/main/java/com/jam3iat/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+//// TODO: download git - config git - add realm DB class 
 public class MainActivity extends AppCompatActivity {
 
     @Override
